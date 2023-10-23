50e0aab4a09adb7d5a86fc18fb321a9fcfad821a
Bilal Muhammad Nura <145457188+bilboy11@users.noreply.github.com>
2023-10-23 11:52:28 +0100

    Delete git.java

--- git.java
+++
@@ -1,8 +0,0 @@
-public class git {
-    public static void main(String[] args) {
-        int car = 10;
-        int bike = 20;
-        int total = car + bike;
-        System.out.println(total);
-    }
-}
